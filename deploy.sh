7bc97417ac9364f9b5f3fa93525dd2959e772254
sosongo <renzo.jca@gmail.com>
2018-01-26 11:34:13 -0300

    Add config test deploy GKE

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ for f in k8s/*.yml
 do
 	envsubst < $f > "generated-$(basename $f)"
 done
-gcloud docker -- push asia.gcr.io/${PROJECT_NAME}/sample:$CIRCLE_SHA1
+gcloud docker -- push us.gcr.io/${PROJECT_NAME}/sample:$CIRCLE_SHA1
 kubectl apply -f generated-deployment.yml --record
 kubectl apply -f generated-service.yml
