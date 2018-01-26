15bc45cd6673e4c9279a4e31d418662773e193a1
sosongo <renzo.jca@gmail.com>
2018-01-26 13:11:47 -0300

    Edit .yml

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ for f in k8s/*.yml
 do
 	envsubst < $f > "generated-$(basename $f)"
 done
+
 gcloud docker -- push us.gcr.io/${PROJECT_NAME}/sample:$CIRCLE_SHA1
-#kubectl apply -f generated-deployment.yml --record
-#kubectl apply -f generated-service.yml
+kubectl apply -f generated-deployment.yml --record
+kubectl apply -f generated-service.yml
